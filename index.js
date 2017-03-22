b3707c8d0534fe1a90e8b242e31acb692c25e6ab
Guillem Cruz <gcquiroga@gmail.com>
2017-03-22 17:46:06 +0000

    No es pot fer click mes de una vgada cada mig segon

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var rpio = require('rpio');
+var _ = require('lodash');
 
 rpio.init();
 
 rpio.open(40, rpio.INPUT,rpio.PULL_UP);
 
-rpio.poll(40, function(pin) {
-    console.log('pressed');
-}, rpio.POLL_LOW);
+rpio.poll(40, _.throttle(function(pin) {
+    console.log('pressed', Date.now());
+}, 500, {trailing: false}), rpio.POLL_LOW);
 
